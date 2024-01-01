9a38146ada9bd5f26caf67d3c5f4ac42c16a7981
AshishVGajbhiye <ashish.gajbhiye@gmail.com>
2024-01-01 11:56:07 +0530

    store and helper function added

---
+++ store_index.py
@@ -0,0 +1,27 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+print(PINECONE_API_KEY) # print api key
+print(PINECONE_API_ENV) # print api env
+
+
+extracted_data = load_pdf("Data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+#Initializing the Pinecone
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+index_name="specchatbot"
+
+#Creating Embeddings for Each of The Text Chunks & storing
+docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
